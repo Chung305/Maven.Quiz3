cb3273ea8fe0c0042c61c0f9ddedd69bd0848cff
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-11-24 15:16:31 -0500

    quick change

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author leon on 09/12/2018.
  */
@@ -9,11 +12,18 @@ public class VowelUtils {
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        for(int i = 0; i < word.length(); i++){
-            if(hasVowels(String.valueOf(word.charAt(i)).toLowerCase())){
-                return i;
-            }
-        }
+//        for(int i = 0; i < word.length(); i++){
+//            if(hasVowels(String.valueOf(word.charAt(i)).toLowerCase())){
+//                return i;
+//            }
+//        }
+//        return -1;
+        Pattern p = Pattern.compile("[aeiou]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(word);
+
+        if(m.find())
+            return m.start();
+
         return -1;
     }
 
